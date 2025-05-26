06911f0cc64a317a5bf54509717b03a47ef712db
Pedro-phd <pedro_dominici@outlook.com>
2025-05-26 15:33:50 -0300

    Adicionar configuração de website estático e política de acesso público ao bucket S3

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -6,7 +6,6 @@ variable "bucket_name" {
   type = string
 }
 
-
 resource "aws_s3_bucket" "static_site_bucket" {
   bucket = "static-site-bucket-${var.bucket_name}"
   tags = {
@@ -15,6 +14,20 @@ resource "aws_s3_bucket" "static_site_bucket" {
   }
 }
 
+# Configuração de website estático
+resource "aws_s3_bucket_website_configuration" "static_site_bucket_website" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Configuração de acesso público
 resource "aws_s3_bucket_public_access_block" "static_site_bucket_access" {
   bucket = aws_s3_bucket.static_site_bucket.id
 
@@ -24,15 +37,32 @@ resource "aws_s3_bucket_public_access_block" "static_site_bucket_access" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_ownership_controls" "static_site_bucket_ownership" {
-  bucket = aws_s3_bucket.static_site_bucket.id
+# Política do bucket para acesso público (substitui o ACL)
+resource "aws_s3_bucket_policy" "static_site_bucket_policy" {
+  bucket     = aws_s3_bucket.static_site_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.static_site_bucket_access]
 
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site_bucket.arn}/*"
+      }
+    ]
+  })
 }
 
-resource "aws_s3_bucket_acl" "static_site_bucket_acl" {
-  bucket = aws_s3_bucket.static_site_bucket.id
-  acl    = "public-read"
+# Output da URL do website
+output "website_url" {
+  value       = "http://${aws_s3_bucket_website_configuration.static_site_bucket_website.website_endpoint}"
+  description = "URL do site estático"
+}
+
+output "bucket_name" {
+  value       = aws_s3_bucket.static_site_bucket.id
+  description = "Nome do bucket criado"
 }
